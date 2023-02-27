59bdaba55b3fd8f4b6316cd4ab960e0242a9c119
Anastasia <agolovanyova@yandex.ru>
2023-02-27 20:41:41 +0300

    add Задача 27

---
+++ Задача27/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int number = ReadInt("Введите число: ");
+
+
+int ReadInt(string message)
+{
+   Console.Write(message);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummAllDigit(int number)
+{
+   int result = 0;
+   while (number > 0)
+   {
+      result += number% 10;
+      number = number / 10;
+   }
+   return result;
+}
+
+System.Console.WriteLine($"Сумма чисел в цифре {number} = {SummAllDigit(number)}");
