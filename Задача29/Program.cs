e34b4259f10ae0685994725d0eaff23307f1e805
Anastasia <agolovanyova@yandex.ru>
2023-02-28 12:07:46 +0300

    add Заадача 27

---
+++ Задача29/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+//
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int lenArray = ReadInt("Введите числа через запятую: ");
+int ReadInt(string message)
+{
+   Console.Write(message);
+   return Convert.ToInt32(Console.ReadLine());
+}
+
+int RandomArray( int Lenght, int minValue, int maxValue)
+{
+    int[] array = new int[Lenght];
+    Random random = new Random();
+    for (int i = 0; i < Lenght; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < Lenght; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write("]");
+}
+
+int[] array = RandomArray(Lenght, min, max);
+PrintArray(array);
